c997957494a5fe1020dfb83ea4a4666f2ab54c09
saka94 <74228457+saka94@users.noreply.github.com>
2021-11-18 14:17:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./sass/style.scss";
 import Navigation from "./components/Navigation";
+import Imagecarousel from "./components/Imagecarousel";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Imagecarousel />
     </>
   );
 }
